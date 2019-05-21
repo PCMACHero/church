b0ee436ed7e78363f8d5de3b660b3797952d9365
PCMACHERO <pcmacmen@gmail.com>
2019-05-21 13:22:52 -0700

    Updates

---
+++ precache-manifest.cac74453af805b2eac621ba19922a577.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "659630c74013ad416ad5",
+    "url": "/church/static/css/main.a46dd59a.chunk.css"
+  },
+  {
+    "revision": "659630c74013ad416ad5",
+    "url": "/church/static/js/main.71153035.chunk.js"
+  },
+  {
+    "revision": "3a3cacbd0a367dc33da0",
+    "url": "/church/static/js/runtime~main.e87ff03c.js"
+  },
+  {
+    "revision": "bdba3ad5f32a9f9b4197",
+    "url": "/church/static/js/2.5983cdb4.chunk.js"
+  },
+  {
+    "revision": "ff91c381bd72549e7b4eb80bece25db4",
+    "url": "/church/static/media/church-logo.ff91c381.jpg"
+  },
+  {
+    "revision": "3451a4a0d3e5c04f7d531825227abd82",
+    "url": "/church/static/media/member-carlos.3451a4a0.jpeg"
+  },
+  {
+    "revision": "c5507dccdbc8a17596f7a8cdf1cd237a",
+    "url": "/church/static/media/Jose-e-Irma-silva.c5507dcc.jpg"
+  },
+  {
+    "revision": "307fb38cab43b5d689d88892a0ba1f84",
+    "url": "/church/static/media/pastor-cesar-preciado.307fb38c.jpeg"
+  },
+  {
+    "revision": "2e4027f8028e7aac5242f78d9a08fd24",
+    "url": "/church/static/media/ministerio-mujer.2e4027f8.jpg"
+  },
+  {
+    "revision": "44a9c52b3360138bcf1ec1aceddbc147",
+    "url": "/church/static/media/ministerio-jovenes.44a9c52b.jpg"
+  },
+  {
+    "revision": "c1d3124142309ff6467850e96a0bb25d",
+    "url": "/church/static/media/ministerio-ninos.c1d31241.jpg"
+  },
+  {
+    "revision": "bb38c5be57820060c986df489e857756",
+    "url": "/church/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8ee69bd5890bdff09f8d250ef5d06b7.js"
+  "/church/precache-manifest.cac74453af805b2eac621ba19922a577.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/church/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
